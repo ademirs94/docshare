3e70ea4638eac7212c2365f91c3510b011b34116
ademirs94 <ademir.s.94@hotmail.com>
2025-12-06 18:50:43 +0000

    fix: remove unused urls

--- core/urls.py
+++ core/urls.py
@@ -10,13 +10,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('upload/', upload_document, name='upload'),
-    # path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('login/', login_view, name='login'),
     path('verify-totp/', verify_totp, name='verify_totp'),
     path('setup-totp/', setup_totp, name='setup_totp'),
-    path('upload-success/', lambda r: render(r, 'upload_success.html'), name='upload_success'),
     path('signup/', signup, name='signup'),
-    path('logout/', logout_view, name='logout'),
     path('my-documents/', user_document_list, name='user_document_list'),
     path('download/<int:document_id>/', download_document, name='download_document'),
     path('document/<int:document_id>/', get_document, name='get_document'),
